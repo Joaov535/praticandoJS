feb66d7c0a32a099f05cac164ab33ec31b4c644b
Joaov535 <joaovictorcruz535@gmail.com>
2022-10-09 13:17:20 -0300

    contador melhorado

---
+++ Aula 14/contador correto/js.js
@@ -0,0 +1,26 @@
+function iniciarContagem() {
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Dados incompletos')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('[ERRO] Passo não pode ser 0 </br> Assumiremos passo valendo 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem progressiva
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{2714}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
+    }
+}
--- Aula 14/ex016/js.js
+++ Aula 14/ex016/js.js
@@ -1,4 +1,5 @@
 function iniciarContagem() {
+    //Declaração de variáveis
     let start = document.getElementById('inicio')
     let inicio = Number(start.value)
     let end = document.getElementById('fim')
@@ -9,14 +10,16 @@ function iniciarContagem() {
     res.innerHTML = 'Contando: '
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerText = 'Impossível contar'
+        window.alert('Impossível contar')
     } else if (inicio % 1 !== 0 || fim % 1 !== 0 || passo % 1 !== 0) {
         alert('Digíte números inteiros')
+        //contagem regressiva
     } else if (inicio >= fim) {
         do {
             res.innerHTML += `${inicio} `
             inicio -= passo
         } while (fim <= inicio)
+        //contagem progressiva
     } else {
         do {
             res.innerHTML += `${inicio} `
